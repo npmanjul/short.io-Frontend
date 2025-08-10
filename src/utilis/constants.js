abfd45346921b2190f8662869350c1107d46327d
Anjul Singh Sachan <anjulsingh462@gmail.com>
2025-08-10 15:48:08 +0530

    chnage link

--- src/utilis/constants.js
+++ src/utilis/constants.js
@@ -1,4 +1,4 @@
 export const BACKEND_URL = "https://lynq-one.vercel.app/api/v1";
 // export const BACKEND_URL = "http://localhost:8000/api/v1";
 // export const FRONTEND_URL = "http://localhost:5173";
-export const FRONTEND_URL = "https://lynq-amber.vercel.app";
+export const FRONTEND_URL = "https://shrt-five.vercel.app";
